ac190e2c6b49e5c942c86c41ac08f27ad5279e17
zmaraj <zsmaraj99@gmail.com>
2025-12-24 14:46:38 -0500

    Upload images and endpoints

---
+++ client/src/components/newPrompt/NewPrompt.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useRef, useState } from 'react';
+import './newPrompt.css'
+import Upload from '../upload/Upload';
+import { IKImage } from 'imagekitio-react';
+
+const NewPrompt = () => {
+
+    const[img,setImg] = useState({
+        isLoading: false,
+        error: "",
+        dbData: {}
+    })
+
+    const endRef = useRef(null);
+
+    useEffect(() => {
+        endRef.current.scrollIntoView({ behavior: "smooth" });
+    },[]);
+
+    return (
+        <>
+        {/* ADD A NEW CHAT */}
+        {img.isLoading && <div className=''>Loading...</div>}
+        {img.dbData?.filePath && (
+            <IKImage
+            urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
+            path={img.dbData?.filePath}
+            width="380"
+            transformation={[{ width: 380}]}
+            />
+        )}
+        <div className="endChat" ref={endRef}></div>
+            <form className="newForm">
+                <Upload setImg={setImg}/>
+                <input id="file" type="file" multiple={false} hidden/>
+                <input type="text" placeholder='Ask anything...'/>
+                <button>
+                    <img src="/arrow.png" alt="" />
+                </button>
+            </form>
+        </>
+    );
+};
+
+export default NewPrompt
--- src/routes/chatPage/ChatPage.jsx
+++
@@ -1,9 +0,0 @@
-import './chatPage.css'
-
-const ChatPage = () => {
-    return (
-        <div className='chatPage'>ChatPage</div>
-    )
-}
-
-export default ChatPage
